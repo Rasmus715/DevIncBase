3b33bd5cc24d0e72cad1c9a5e13f905283c5bf13
Ivan Zatisev <i.zatsev@dreamsoft.by>
2022-07-12 18:00:50 +0300

    Add splashscreen

---
+++ SeaBattle/SeaBattleGame.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ivan Zaitsev on 7/12/22.
+//
+
+#include "SplashScreen.h"
+
+using namespace std;
+
+
+void SplashScreen::Print() {
+    cout << "   _____                  ____        _   _   _          " << endl;
+    cout << R"(  / ____|                |  _ \\      | | | | | |        )" << endl;
+    cout << " | (___   ___  __ _      | |_) | __ _| |_| |_| | ___     " << endl;
+    cout << R"(  \___ \ / _ \/ _` |     |  _ < / _` | __| __| |/ _ \   )" << endl;
+    cout << "  ____) |  __/ (_| |     | |_) | (_| | |_| |_| |  __/   " << endl;
+    cout << R"( |_____/ \___|\__,_|     |____/ \__,_|\__|\__|_|\___|   )" << endl;
+    cout << endl;
+    cout << "                       Press \"Enter\" key                    " << endl;
+}
---
+++ SeaBattle/SeaBattleGame.h
@@ -0,0 +1,16 @@
+//
+// Created by Ivan Zaitsev on 7/12/22.
+//
+
+#ifndef UNTITLED_SPLASHSCREEN_H
+#define UNTITLED_SPLASHSCREEN_H
+
+#include <iostream>
+
+class SplashScreen {
+
+public:
+    void Print();
+};
+
+#endif //UNTITLED_SPLASHSCREEN_H
