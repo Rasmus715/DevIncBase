2d08fe04e6ca7f4f5f1241a0ba106bee1fb94ea4
macevilamv <maksim.matsevila@gmail.com>
2022-07-12 16:06:21 +0300

    done chesstasks

---
+++ horses/horses.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int yAxis1 = 0;
+    int xAxis1 = 0;
+    int yAxis2 = 0;
+    int xAxis2 = 0;
+    bool answer = false;
+
+    cout << "Enter coordinates X for the first horse: ";
+    cin >> xAxis1;
+
+    cout << "Enter coordinates Y for the first horse: ";
+    cin >> yAxis1;
+
+    cout << "Enter coordinates X for the second horse: ";
+    cin >> xAxis2;
+
+    cout << "Enter coordinates Y for the second horse: ";
+    cin >> yAxis2;
+
+    	
+    if (xAxis1 - 2 == xAxis2 and yAxis1 + 1 == yAxis2) {
+            answer = true;
+    }
+
+    if (xAxis1  + 2 == xAxis2 and yAxis1 - 1 == yAxis2) {
+        	answer = true;
+    }
+        
+    if(xAxis1 + 1 == xAxis2 and yAxis1 - 2 == yAxis2) {
+        	answer = true;
+    }
+    
+    if(xAxis1 - 1 == xAxis2 and yAxis1 - 2 == yAxis2) {
+        	answer = true;
+    }
+    
+    if(xAxis1 -2 == xAxis2 and yAxis1 - 1 == yAxis2) {
+        	answer = true;
+    }
+    
+    if(xAxis1 + 1 == xAxis2 and yAxis1 + 2 == yAxis2) {
+        	answer = true;
+    }
+    
+    if(xAxis1 + 2 == xAxis2 and yAxis1 - 1 == yAxis2) {
+        	answer = true;
+    }
+       
+    if(xAxis1 - 1 == xAxis2 and yAxis1 + 2 == yAxis2) {
+        	answer = true;
+    }
+            
+
+    if (answer) {
+        cout << "Horses can beat one another!";
+    } else {
+        cout << "Horses can't beat one another!";
+    }
+
+    return 0;
+}
