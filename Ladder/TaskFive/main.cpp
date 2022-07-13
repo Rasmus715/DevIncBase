7aebe68db3dc78745a611933202a1b1d76577150
macevilamv <106157193+macevilamv@users.noreply.github.com>
2022-07-13 10:29:15 +0300

    Update main.cpp
    minor codestyle fixes

--- Ladder/TaskFive/main.cpp
+++ Ladder/TaskFive/main.cpp
@@ -7,9 +7,9 @@ int main()
     int number = 0;
     int biggestDigit = 0;
     int smallestDigit = 0;
-    int avgSum = 0;
+    int averageSum = 0;
     int numberOfDigits = 0;
-    int avg = 0;
+    int average = 0;
 
     cout << "Please, enter the number: ";
     cin >> number;
@@ -22,19 +22,19 @@ int main()
         if (biggestDigit < number % 10)
             biggestDigit = number % 10;
 
-        if(smallestDigit > number % 10)
+        if (smallestDigit > number % 10)
             smallestDigit = number % 10;
 
-        avgSum += number % 10;
+        averageSum += number % 10;
         numberOfDigits++;
         number /= 10;
-
     }
 
-    avg = avgSum / numberOfDigits;
+    average = averageSum / numberOfDigits;
+    
     cout << "Biggest digit in number: " << biggestDigit << endl;
     cout << "Smallest digit in number: " << smallestDigit << endl;
-    cout << "Average digit in number: " << avg << endl;
+    cout << "Average digit in number: " << average << endl;
 
     return 0;
 }
