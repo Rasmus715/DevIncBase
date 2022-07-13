7df591ad37c4d61970332207690c3d04a1a97f3a
macevilamv <106157193+macevilamv@users.noreply.github.com>
2022-07-13 11:04:22 +0300

    incorrect input checked

--- bishops/bishops.cpp
+++ bishops/bishops.cpp
@@ -21,9 +21,13 @@ int main()
 
     cout << "Enter coordinates Y for the second bishop: ";
     cin >> yAxis2;
-
+    
+    if ((xAxis1 > 8 or xAxis1 < 1) or (yAxis1 > 8 or yAxis2 < 1) or (xAxis2 > 8 or xAxis1 < 1) or (yAxis1 > 8 or yAxis2 < 1)) {
+		cout << "Error: Wrong coordinates. Use numbers < 9 and > 0" << endl;
+		return 1;
+	}
+	
     for (int i = 0; i < 8; i++) {
-        
         if (xAxis1 + i == xAxis2 and yAxis1 + i == yAxis2) {
             answer = true;
         }
