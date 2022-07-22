b0a3464a35d810a646cfe062e59854c3e985f29b
Ivan Zatisev <i.zatsev@dreamsoft.by>
2022-07-22 17:29:22 +0300

    fix: fix rotation solution

--- SprintTwo/Rotation/main.cpp
+++ SprintTwo/Rotation/main.cpp
@@ -5,15 +5,23 @@ using namespace std;
 const int ZERO = 0;
 const int ONE = 1;
 
+int binaryNumber[32];
+int binaryNumberSize = 0;
+
 int enterAndValidateNumber();
-bool isNumberRotating(long number);
+bool isNumberRotating();
+void printBinaryNumber(int binaryNumber[], int number);
+void convertToBinary(int number);
 
 int main()
 {
-    long number = 0;
+    int number = 0;
 
     number = enterAndValidateNumber();
-    if(isNumberRotating(number))
+    convertToBinary(number);
+    printBinaryNumber(binaryNumber, number);
+
+    if(isNumberRotating())
         cout << "The number " << number << " is rotating";
     else
         cout << "The number " << number << " is not rotating";
@@ -27,43 +35,52 @@ int enterAndValidateNumber()
 
     do
     {
-        cout << "Enter the number of sequence: ";
+        cout << "Enter the number: ";
         cin >> result;
         if(result < 0)
         {
-            cout << "Incorrect input. The number of sequence cannot be less than zero" << endl;
-            cout << "Enter the number of sequence: ";
+            cout << "Incorrect input. The number cannot be less than zero" << endl;
         }
     }
     while(result < 0);
     return result;
 }
 
-bool isNumberRotating(long number)
+bool isNumberRotating()
 {
-    long lastDigit = 0;
 
-    while(number > 9)
+    for (int j = binaryNumberSize - 1; j >= 1; j--)
     {
-        lastDigit = number % 10;
-
-        if(lastDigit != ONE && lastDigit != ZERO)
+        if(binaryNumber[j] == ZERO && binaryNumber[j-1] != ONE)
         {
             return false;
         }
 
-        if(lastDigit == ZERO && number / 10 % 10 != ONE)
+        if(binaryNumber[j] == ONE && binaryNumber[j-1] != ZERO)
         {
             return false;
         }
-
-        if(lastDigit == ONE && number / 10 % 10 != ZERO)
-        {
-            return false;
-        }
-
-        number /= 10;
     }
 
     return true;
+
 }
+
+void printBinaryNumber(int binaryNumber[], int number)
+{
+    cout <<"The number " << number << " in binary is: ";
+    for (int j = binaryNumberSize - 1; j >= 0; j--)
+        cout << binaryNumber[j];
+    cout << endl;
+}
+
+void convertToBinary(int number)
+{
+    while (number > 0)
+    {
+        binaryNumber[binaryNumberSize] = number % 2;
+        number = number / 2;
+        binaryNumberSize++;
+    }
+}
+
